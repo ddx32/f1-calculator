d85daea6fef272a634b8103e8ccd4b4756e1cc51
Filip Růžička <f@filipruzicka.com>
2022-09-10 02:15:18 +0200

    Fix sprint race fastest lap calculation

--- src/services/pointsCalculations.ts
+++ src/services/pointsCalculations.ts
@@ -13,6 +13,11 @@ export function getPointsPerRace(
     raceType === RaceType.GRAND_PRIX
       ? POINTS_PER_POSITION.fullGrandPrix[index]
       : POINTS_PER_POSITION.sprintRace[index];
+
+  if (raceType === RaceType.SPRINT_RACE) {
+    return positionPoints || 0;
+  }
+
   return index < 10 && fastestLap ? positionPoints + 1 : positionPoints || 0;
 }
 
