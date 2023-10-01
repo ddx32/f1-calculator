232b04f386239fc4b4c411658f24c88bec214639
Filip Růžička <f@filipruzicka.com>
2023-10-01 19:36:20 +0200

    Add caching proxy for API requests

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,4 +1,7 @@
-export const BASE_URL = "https://ergast.com/api/f1";
+// Use cached API for production
+export const BASE_URL = import.meta.env.DEV
+  ? "https://ergast.com/api/f1"
+  : "/api";
 
 export const PATH = {
   driverStandings: "current/driverStandings.json",
