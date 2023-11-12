e7ab337adbdee58621a174745a9049e34c8d0bba
Filip Růžička <f@filipruzicka.com>
2023-11-12 17:16:44 +0100

    Disable API proxy temporarily

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,9 @@
 // Use cached API for production
-export const BASE_URL = import.meta.env.DEV
-  ? "https://ergast.com/api/f1"
-  : "/api";
+// export const BASE_URL = import.meta.env.DEV
+//   ? "https://ergast.com/api/f1"
+//   : "/api";
+
+export const BASE_URL = "https://ergast.com/api/f1";
 
 export const PATH = {
   driverStandings: "current/driverStandings.json",
